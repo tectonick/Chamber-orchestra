93053598863693131881abc067ae90e272f2235f
Nick <korn9509@gmail.com>
2020-08-16 23:20:20 +0300

    Concerts page with SQL

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express=require("express");
 const handlebars=require("express-handlebars");
 const router=require("./routes/default");
-const mysql = require("mysql2");
+
 
 const PORT = process.env.PORT || 5000;
 const app=express();
@@ -9,23 +9,6 @@ const hbs=handlebars.create({
   defaultLayout:"main",
   extname:"hbs"
 });
-  
-const connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "devpassword123",
-  database:"chamber"
-});
-
-connection.connect((err)=>{
-  if (err) {
-    return console.error("Error connecting to MySQL: " + err.message);
-  }
-  else{
-    console.log("Connected to MySQL");
-  }
-})
-
 
 app.engine("hbs", hbs.engine);
 app.set("view engine", "hbs");
