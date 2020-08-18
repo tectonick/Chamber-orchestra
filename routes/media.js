80d00a87b89e561fe6fbff6e09d886a1b397f815
Nick <korn9509@gmail.com>
2020-08-19 01:16:24 +0300

    page transitions and gallery

--- routes/media.js
+++ routes/media.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const router = express.Router();
+const viewhelpers = require("../viewhelpers");
 
 
 
-router.get("/photos", (req,res)=>{
-    res.render('media/photos.hbs');
+router.get("/photos", async (req,res)=>{
+    
+    var names= await viewhelpers.NamesOfDirFilesWOExtension("/static/img/gallery");
+    res.render('media/photos.hbs',{names});
 });
 router.get("/videos", (req,res)=>{
     res.render('media/videos.hbs');
