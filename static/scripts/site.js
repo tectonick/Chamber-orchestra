c8e55334b17bd8841be231b86e3aee1927a61b85
Nick <korn9509@gmail.com>
2020-08-22 21:23:05 +0300

    sticky adaptive navbar

--- static/scripts/site.js
+++ static/scripts/site.js
@@ -13,3 +13,14 @@
         document.getElementById('page-container').style.opacity=1;
       
       });
+
+
+      window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 40 || document.documentElement.scrollTop > 40) {
+    document.getElementById("logo").style.width = "100px";
+  } else {
+    document.getElementById("logo").style.width = "180px";
+  }
+}
