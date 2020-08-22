7230087f95d5cc921b1aa6f8194dc20f7adf9cd8
Nick <korn9509@gmail.com>
2020-08-22 23:05:34 +0300

    localization started

--- app.js
+++ app.js
@@ -8,17 +8,55 @@ const path=require('path');
 const cookieParser = require('cookie-parser');
 const fileUpload = require('express-fileupload');
 const session = require('express-session');
+const i18next = require('i18next');
+const i18nextMiddleware = require('i18next-express-middleware');
+const Backend = require('i18next-node-fs-backend');
+
+
+
+i18next
+    .use(Backend)
+    .use(i18nextMiddleware.LanguageDetector)
+    .init({
+      backend: {
+        loadPath: path.join(__dirname, '/locales/{{lng}}/{{ns}}.json'),
+      },
+      detection: {
+        order: ['querystring', 'cookie'],
+        caches: ['cookie']
+      },
+      fallbackLng: 'ru',
+      preload: ['en', 'ru']
+    });
+
+
+
+
+
+
+
 
 const PORT = process.env.PORT || 5000;
 const app=express();
 
 const hbs=handlebars.create({
   defaultLayout:"main",
-  extname:"hbs"
+  extname:"hbs",
+  helpers: {
+    t: function(str){
+      return (i18next != undefined ? i18next.t(str) : str);
+    
+    }
+  }
 });
+
 app.engine("hbs", hbs.engine);
 app.set("view engine", "hbs");
 
+app.use(i18nextMiddleware.handle(i18next, {
+      ignoreRoutes: ["/foo"], // or function(req, res, options, i18next) { /* return true to ignore */ }
+      removeLngFromUrl: false
+    }));
 app.use(session({secret: 'ssshhhhh'}));
 app.use(cookieParser());
 app.use(fileUpload());
