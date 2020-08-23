7566b552f949287b57df5ff8f7cafc195ad69c22
Nick <korn9509@gmail.com>
2020-08-23 04:04:19 +0300

    heroku db

--- db.js
+++ db.js
@@ -3,10 +3,10 @@ const mysql = require("mysql2");
 
 //db connection
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "devpassword123",
-  database: "chamber"
+  host: "eu-cdbr-west-03.cleardb.net",
+  user: "bd9350e491a479",
+  password: "0da285ca",
+  database: "heroku_30bd9ac110e06b7"
 });
 db.connect((err) => {
   if (err) {
