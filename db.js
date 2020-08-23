2d4c0d74c1ae0ffa07a35552dc6f096d33712ca9
Nick <korn9509@gmail.com>
2020-08-23 05:55:27 +0300

    changed db to pool mode

--- db.js
+++ db.js
@@ -2,20 +2,23 @@ const mysql = require("mysql2");
 
 
 //db connection
-const db = mysql.createConnection({
+const db = mysql.createPool({
   host: "localhost",
   user: "belscone_root",
   password: "M51cT4n5qb",
-  database: "belscone_chamber"
+  database: "belscone_chamber",
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
-db.connect((err) => {
-  if (err) {
-    return console.error("Error connecting to MySQL: " + err.message);
-  }
-  else {
-    console.log("Connected to MySQL");
-  }
-});
+// db.connect((err) => {
+//   if (err) {
+//     return console.error("Error connecting to MySQL: " + err.message);
+//   }
+//   else {
+//     console.log("Connected to MySQL");
+//   }
+// });
 
 
 module.exports=db;
