823786ea2f5da4fcec98957d34c95ad1977963af
Nick <korn9509@gmail.com>
2020-09-23 19:30:13 +0300

    fixed pwa and deleted debug messages

--- static/sw_cached_site.js
+++ static/sw_cached_site.js
@@ -2,12 +2,10 @@ const cacheName = 'v1';
 
 // Call Install Event
 self.addEventListener('install', e => {
-  console.log('Service Worker: Installed');
 });
 
 // Call Activate Event
 self.addEventListener('activate', e => {
-  console.log('Service Worker: Activated');
   // Remove unwanted caches
   e.waitUntil(
     caches.keys().then(cacheNames => {
@@ -25,13 +23,11 @@ self.addEventListener('activate', e => {
 
 // Call Fetch Event
 self.addEventListener('fetch', e => {
-  console.log('Service Worker: Fetching');
   e.respondWith(
     fetch(e.request)
       .then(res => {
         // Make copy/clone of response
         const resClone = res.clone();
-        console.log(resClone);
         // Open cahce
         caches.open(cacheName).then(cache => {
           // Add response to cache
