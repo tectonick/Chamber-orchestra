0f958fbbb2784a6ab37a67307c3ee6a84082a316
Tectonick <korn9509@gmail.com>
2022-05-01 13:43:20 +0300

    Calendar links for archive with pagination fix

--- routes/events.js
+++ routes/events.js
@@ -24,7 +24,16 @@ router.get("/", async (req, res, next) => {
 router.get("/archive", async (req, res, next) => {
   var description = res.__("archive.description");
   var title = res.__("layout.navbar.archive") + " | " + res.__("title");
+
   try {
+    if (req.query.id){
+      let [results] = await db.query(
+        `SELECT * FROM concerts WHERE id=${Number(req.query.id)}`
+      );
+      let months = viewhelpers.OrganizeConcertsInMonths(results);
+      res.render("events/archive.hbs", { pages:[], months, title, description });
+      return;
+    }
     let [countDbResult] = await db.query(`SELECT COUNT(id) as count FROM concerts WHERE date<NOW() AND date!='1970-01-01 00:00:00'`);
     let maxCount=countDbResult[0].count;
     let {pages, itemCount, offset}=viewhelpers.usePagination("archive",req.query.page,maxCount,config.get("paginationSize").archive);
@@ -34,7 +43,7 @@ router.get("/archive", async (req, res, next) => {
     results.forEach((element) => {
       element.description = viewhelpers.UnescapeQuotes(element.description);
     });
-    var months = viewhelpers.OrganizeConcertsInMonths(results);
+    let months = viewhelpers.OrganizeConcertsInMonths(results);
     res.render("events/archive.hbs", { pages, months, title, description });
   } catch (error) {
     next(error);
